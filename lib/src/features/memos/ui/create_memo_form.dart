5337172e0cbf8650d657406fae42283dca13a6f7
Brian Karfunkel <bkarfunkel@gmail.com>
2024-05-24 08:58:57 -0400

    Make create memo form actually submit

--- lib/src/features/memos/ui/create_memo_form.dart
+++ lib/src/features/memos/ui/create_memo_form.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:untask/src/common/ui/choice_chip_set.dart';
-import 'package:untask/src/features/memos/ui/create_memo_form_controller.dart';
+import 'package:untask/src/features/memos/data/memos_repository.dart';
 import 'package:untask/src/features/motivations/data/motivations_repository.dart';
+
+import '../models/memo.dart';
 
 class CreateMemoForm extends ConsumerStatefulWidget {
   const CreateMemoForm({super.key});
@@ -13,39 +17,40 @@ class CreateMemoForm extends ConsumerStatefulWidget {
 
 // IDEA: We should use the same form to both display and edit a memo?
 class _CreateMemoFormState extends ConsumerState<CreateMemoForm> {
-  final _formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormBuilderState>();
 
   @override
   Widget build(BuildContext context) {
-    final state = ref.watch(createMemoFormControllerProvider);
-
-    final motivations = ref.watch(motivationsListProvider);
-    debugPrint("Motivations: ${motivations.toString()}");
-    return Form(
+    return FormBuilder(
       key: _formKey,
       child: Padding(
         padding: const EdgeInsets.all(25.0),
         child: Column(
           children: <Widget>[
-            const Text('Title'),
-            TextFormField(
-              enabled: !state.isLoading,
-              initialValue: "Foo",
+            FormBuilderTextField(
+              name: 'Memo Name',
+              autofocus: true,
+              decoration: const InputDecoration(labelText: 'Thing to do'),
+              validator: FormBuilderValidators.compose([
+                FormBuilderValidators.required(),
+                FormBuilderValidators.minLength(3),
+              ]),
             ),
-            const Text('I can do this in...'),
-            motivations.when(
-              data: (motivs) {
-                debugPrint("Motivations: ${motivs.toString()}");
-                return ChoiceChipSet(
-                    choices: motivs
-                        .map((e) => (label: e.description, value: e.id))
-                        .toList());
-              },
-              loading: () => const Text('loading'),
-              error: (error, stackTrace) => const Text('Error'),
-            ),
+            // const Text('I can do this in...'),
+            // motivations.when(
+            //   data: (motivs) {
+            //     debugPrint("Motivations: ${motivs.toString()}");
+            //     return ChoiceChipSet(
+            //         choices: motivs
+            //             .map((e) => (label: e.description, value: e.id))
+            //             .toList());
+            //   },
+            //   loading: () => const Text('loading'),
+            //   error: (error, stackTrace) => const Text('Error'),
+            // ),
             // LATER: Add a "+" button to add more motivations
-            ElevatedButton(onPressed: _submit, child: const Text('Submit'))
+            const Spacer(),
+            ElevatedButton(onPressed: _submit, child: const Text('Submit')),
           ],
         ),
       ),
@@ -53,13 +58,27 @@ class _CreateMemoFormState extends ConsumerState<CreateMemoForm> {
   }
 
   Future<void> _submit() async {
-    // NOW: When a create memo form is submitted, it should create the memo in the repository
-    setState(() {});
-    _formKey.currentState?.save();
-    final controller = ref.read(createMemoFormControllerProvider.notifier);
-    final success = await controller.submit();
-    final value = _formKey.currentState.toString();
-    debugPrint("Success: $value $success");
-    // TODO add params to submit
+    // Validate and save the form values
+    if (_formKey.currentState?.saveAndValidate() ?? false) {
+      debugPrint("Valid: ${_formKey.currentState?.value.toString()}");
+      ref.read(memosRepositoryProvider).createMemo(
+            Memo(title: _formKey.currentState!.value['Memo Name'] as String),
+          );
+    } else {
+      debugPrint("Invalid: ${_formKey.currentState?.value.toString()}");
+    }
+    Navigator.of(context).pop();
+
+    // On another side, can access all field values without saving form with instantValues
+    // _formKey.currentState?.validate();
+    // debugPrint(_formKey.currentState?.instantValue.toString());
+    //   // NOW: When a create memo form is submitted, it should create the memo in the repository
+    //   setState(() {});
+    //   _formKey.currentState?.save();
+    //   final controller = ref.read(createMemoFormControllerProvider.notifier);
+    //   final success = await controller.submit();
+    //   final value = _formKey.currentState.toString();
+    //   debugPrint("Success: $value $success");
+    //   // TODO add params to submit
   }
 }
